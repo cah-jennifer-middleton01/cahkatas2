9f5960a7289f6642e36ea47cc1118053c38b3a1f
jennifer.middleton01 <jennifer.middleton01@cardinalhealth.com>
2020-09-04 16:31:02 -0400

    REFACTOR: Streamlined the two methods

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,5 +1,23 @@
 public class Fibonacci {
-    public static String outputSequence(int i) {
-        return "0, 1, 1, 2, 3, 5, 8, 13, 21, 34";
+    public static String outputSequence(int end) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < end; ) {
+            sb.append(fibonacci(i));
+            i++;
+            if (i != end) {
+                sb.append(", ");
+            }
+        }
+        return sb.toString();
+    }
+
+    private static int fibonacci(int i) {
+        if (i >= 2) {
+            return fibonacci(i - 1) + fibonacci(i - 2);
+        } else if (i == 1) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
